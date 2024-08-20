99a72c8afbad2e03e80bf37014cb66265ce04822
NoLimits <floreslenguaeduardo@gmail.com>
2024-08-20 17:11:55 -0500

    v1.0.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore_credentials: {
+    projectId: 'red-social-bfd07',
+    appId: '1:908629705294:web:1e27c3e68e102b9f8b791f',
+    storageBucket: 'red-social-bfd07.appspot.com',
+    apiKey: 'AIzaSyCaEi5CDPv4shAcUbd8W_vVgXZ285iBdtY',
+    authDomain: 'red-social-bfd07.firebaseapp.com',
+    messagingSenderId: '908629705294',
+  }
 };
